7ec914e2c6f7eb5d1971c070fb4df7373a8df674
taehun_jang <gmco_ceo@daum.net>
2022-05-09 13:54:56 +0900

    1.0

--- main.js
+++ main.js
@@ -6,45 +6,61 @@ let percentAnimation3 = document.querySelector('#percent-animation-3')
 let percentAnimation4 = document.querySelector('#percent-animation-4')
 let textAnimation = document.querySelector('#text-animation')
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     let value = window.scrollY;
     console.log("scrollY", value);
 
-    if ( screen.width > 1200 && value > 2400) {
-       percentAnimation1.style.animation = 'percent-1 0.8s ease-in-out';
-       percentAnimation2.style.animation = 'percent-2 1.2s ease-in-out';
-       percentAnimation3.style.animation = 'percent-3 1.5s ease-in-out';
-       percentAnimation4.style.animation = 'percent-4 2s ease-in-out';
-    } else if ( 900 < screen.width < 1200 && value > 1800) {
-        percentAnimation1.style.animation = 'percent-1 0.8s ease-in-out';
-        percentAnimation2.style.animation = 'percent-2 1.2s ease-in-out';
-        percentAnimation3.style.animation = 'percent-3 1.5s ease-in-out';
-        percentAnimation4.style.animation = 'percent-4 2s ease-in-out';
-    } else if ( 500 < screen.width < 900 && value > 2100) {
-        percentAnimation1.style.animation = 'percent-1 0.8s ease-in-out';
-        percentAnimation2.style.animation = 'percent-2 1.2s ease-in-out';
-        percentAnimation3.style.animation = 'percent-3 1.5s ease-in-out';
-        percentAnimation4.style.animation = 'percent-4 2s ease-in-out';
-    } else if ( screen.width <= 500 && value > 1400 ) {
-        percentAnimation1.style.animation = 'percent-1 0.8s ease-in-out';
-        percentAnimation2.style.animation = 'percent-2 1.2s ease-in-out';
-        percentAnimation3.style.animation = 'percent-3 1.5s ease-in-out';
-        percentAnimation4.style.animation = 'percent-4 2s ease-in-out';
-    }else {
-        percentAnimation1.style.animation = 'percent-disappear1 0.5s ease-in-out forwards';
-        percentAnimation2.style.animation = 'percent-disappear2 0.5s ease-in-out forwards';
-        percentAnimation3.style.animation = 'percent-disappear3 0.5s ease-in-out forwards';
-        percentAnimation4.style.animation = 'percent-disappear4 0.5s ease-in-out forwards';
+    if (screen.width > 1200 && value > 4000) {
+        percentAnimation1.style.animation = 'percent-1 1s ease-in-out';
+        percentAnimation2.style.animation = 'percent-2 1.5s ease-in-out';
+        percentAnimation3.style.animation = 'percent-3 2s ease-in-out';
+        percentAnimation4.style.animation = 'percent-4 2.5s ease-in-out';
+    } else if (991 < screen.width <= 1200 && value > 3200) {
+        percentAnimation1.style.animation = 'percent-1 1s ease-in-out';
+        percentAnimation2.style.animation = 'percent-2 1.5s ease-in-out';
+        percentAnimation3.style.animation = 'percent-3 2s ease-in-out';
+        percentAnimation4.style.animation = 'percent-4 2.5s ease-in-out';
+    } else if (768 < screen.width <= 991 && value > 3400) {
+        percentAnimation1.style.animation = 'percent-1 1s ease-in-out';
+        percentAnimation2.style.animation = 'percent-2 1.5s ease-in-out';
+        percentAnimation3.style.animation = 'percent-3 2s ease-in-out';
+        percentAnimation4.style.animation = 'percent-4 2.5s ease-in-out';
+    } else if (480 < screen.width <= 768 && value > 2700) {
+        percentAnimation1.style.animation = 'percent-1 1s ease-in-out';
+        percentAnimation2.style.animation = 'percent-2 1.5s ease-in-out';
+        percentAnimation3.style.animation = 'percent-3 2s ease-in-out';
+        percentAnimation4.style.animation = 'percent-4 2.5s ease-in-out';
+    } else if (400 < screen.width <= 480 && value > 2000) {
+        percentAnimation1.style.animation = 'percent-1 1s ease-in-out';
+        percentAnimation2.style.animation = 'percent-2 1.5s ease-in-out';
+        percentAnimation3.style.animation = 'percent-3 2s ease-in-out';
+        percentAnimation4.style.animation = 'percent-4 2.5s ease-in-out';
+    } else if (screen.width <= 400 && value > 2400) {
+        percentAnimation1.style.animation = 'percent-1 1s ease-in-out';
+        percentAnimation2.style.animation = 'percent-2 1.5s ease-in-out';
+        percentAnimation3.style.animation = 'percent-3 2s ease-in-out';
+        percentAnimation4.style.animation = 'percent-4 2.5s ease-in-out';
+    } else {
+        percentAnimation1.style.animation = 'percent-disappear1 1s ease-out forwards';
+        percentAnimation2.style.animation = 'percent-disappear2 1s ease-out forwards';
+        percentAnimation3.style.animation = 'percent-disappear3 1s ease-out forwards';
+        percentAnimation4.style.animation = 'percent-disappear4 1s ease-out forwards';
     }
 
-    if( screen.width > 1200 && value > 1000) {
-        textAnimation.style.animation = 'text-1 1.5s ease-out'
-    }else if (500<screen.width<1200 && value > 600) {
-        textAnimation.style.animation = 'text-1 1.5s ease-out'
-    }else if (this.screen.width<500 && value > 800) {
-        textAnimation.style.animation = 'text-1 1.5s ease-out'
-    }else {
-        textAnimation.style.animation = 'text-disappear 1.5s ease-out'
+    if (screen.width > 1200 && value > 2000) {
+        textAnimation.style.animation = 'text-1 2s ease-out'
+    } else if (991 < screen.width <= 1200 && value > 1400) {
+        textAnimation.style.animation = 'text-1 2s ease-out'
+    } else if (768 < screen.width <= 991 && value > 1800) {
+        textAnimation.style.animation = 'text-1 2s ease-out'
+    } else if (480 < screen.width <= 768 && value > 1350) {
+        textAnimation.style.animation = 'text-1 2s ease-out'
+    } else if (400 < screen.width <= 480 && value > 1100) {
+        textAnimation.style.animation = 'text-1 2s ease-out'
+    } else if (screen.width <= 400 && value > 1300) {
+        textAnimation.style.animation = 'text-1 2s ease-out'
+    } else {
+        textAnimation.style.animation = 'text-disappear 1.5s ease-out forwards'
     }
 });
 
@@ -57,5 +73,16 @@ agreeButton.addEventListener('click', agree)
 
 function agree() {
     window.open("sub_index.html", "PopupWin", "width=" + new_window_width + ", height=" + new_window_height + ", top=" + positionY + ", left=" + positionX);
+}
+
+function birthMaxlength(e) {
+    if (e.value.length > e.maxLength) {
+        e.value = e.value.slice(0, e.maxLength);
+    }
+}
+function telMaxlength(e) {
+    if (e.value.length > e.maxLength) {
+        e.value = e.value.slice(0, e.maxLength);
+    }
 }
 
